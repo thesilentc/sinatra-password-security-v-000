0633a71b734c5a624aba208e87424843ecbb3a89
thesilentc <thesilentc@me.com>
2018-06-23 22:27:36 -0400

    old code not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,15 +18,28 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		#your code here!
-	end
+		user = User.new(:username => params[:username], :password => params[:password])
+
+			if user.save
+	        redirect "/login"
+	    else
+	        redirect "/failure"
+	   		end
+		end
+
 
 	get "/login" do
 		erb :login
 	end
 
-	post "/login" do
-		#your code here!
-	end
+	user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+      redirect "/success"
+    else
+      redirect "/failure"
+    end
+end
 
 	get "/success" do
 		if logged_in?
@@ -54,5 +67,3 @@ class ApplicationController < Sinatra::Base
 			User.find(session[:user_id])
 		end
 	end
-
-end
